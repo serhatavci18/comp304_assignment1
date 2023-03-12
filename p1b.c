f42ad04bbb9a454743283652aba6180510d2309e
Serhat <serhatavci18@ku.edu.tr>
2023-03-12 20:17:53 +0300

    p1b done

--- p1b.c
+++ p1b.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(){
-   __pid_t child_pid;
+   
+   __pid_t child_pid = -1;
 
    child_pid = fork();
 
    if(child_pid == 0){
       printf("Child process ID: %d " , getpid());
+      exit(0);
+      
+   }else{
       sleep(5);
    }
 
